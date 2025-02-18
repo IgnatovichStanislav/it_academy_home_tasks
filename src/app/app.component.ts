06b8ae660323d7e690ad6d3213ae6f27046865de
Stanislav Ignatovich <stanislav.ihnatovich@xplanis.com>
2025-02-18 16:36:55 +0100

    Домашнее задание 2. Создать Git репозиторий и инициализировать в нем Angular приложение (рекомендуемая версия - 19). Сверстать с использованием компонентов пользовательскую карточку Пример карточки  и принцип разбиения на компоненты.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { MoreInfoButtonComponent } from './more-info-button/more-info-button.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInfoComponent, MoreInfoButtonComponent],
 })
 export class AppComponent {
   title = 'angular-home-project';
