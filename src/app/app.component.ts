b1ed2325f0cd601ccfbbb679c6b7bbd7acb8e24d
Stanislav Ignatovich <stanislav.ihnatovich@xplanis.com>
2025-02-18 14:07:12 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [HeaderComponent],
+})
+export class AppComponent {
+  title = 'angular-home-project';
+}
