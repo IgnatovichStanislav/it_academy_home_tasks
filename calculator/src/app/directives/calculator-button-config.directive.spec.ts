d0c3182141145817091cba6d677bd651b79c755b
Stanislav Ignatovich <stanislav.ihnatovich@xplanis.com>
2025-02-23 20:58:18 +0100

    Домашнее Задание 3 *remove directives

--- calculator/src/app/directives/calculator-button-config.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CalculatorButtonConfigDirective } from './calculator-button-config.directive';
-
-describe('CalculatorButtonConfigDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CalculatorButtonConfigDirective();
-    expect(directive).toBeTruthy();
-  });
-});
