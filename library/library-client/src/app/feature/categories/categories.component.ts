92b869044f7e07209fccbefcad18846e953ea7ba
Stanislav Ignatovich <stanislav.ihnatovich@xplanis.com>
2025-04-10 15:19:12 +0200

    fix imports

--- library/library-client/src/app/feature/categories/categories.component.ts
+++ library/library-client/src/app/feature/categories/categories.component.ts
@@ -1,19 +1,21 @@
 import { Component, Inject } from '@angular/core';
-import { Book } from '../../core/models/books/Book';
-import { CategoriesFilterComponent } from './categories-filter/categories-filter.component';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { switchMap } from 'rxjs/internal/operators/switchMap';
 
-import { BooksListComponent } from '../books/books-list/books-list.component';
+import { Book } from '../../core/models/books/Book';
 import { Category } from '../../core/models/categories/Category';
 import { BooksUiDataService } from '../../core/ui-data-services/BooksUiDataService';
 import { IBooksUiDataService } from '../../core/ui-data-services/contracts/IBooksUiDataService';
 import { ICategoriesService } from '../../core/services/contracts/ICategoriesService';
 import { CategoriesService } from '../../core/services/CategoriesService';
 import { BooksFilter } from '../../core/models/books/BooksFilter';
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
-import { switchMap } from 'rxjs/internal/operators/switchMap';
 import { ILoadingService } from '../../core/ui-services/contracts/ILoadingService';
 import { LoadingService } from '../../core/ui-services/LoadingService ';
 import { LoadingIndicatorComponent } from '../../shared/components/loading-indicator/loading-indicator.component';
+
+import { CategoriesFilterComponent } from './categories-filter/categories-filter.component';
+import { BooksListComponent } from '../books/books-list/books-list.component';
+
 @Component({
   selector: 'app-categories',
   imports: [
